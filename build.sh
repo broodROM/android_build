c0e43e40d81b2d95679866db63141e61c035b617
Florent Guichard <guichardflorent@gmail.com>
2013-04-15 19:13:31 +0300

    Update to revision 2

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+
+# Init Vars
 HOMEDIR=${PWD}
 ARIESVEDIR=${HOMEDIR}/out/target/product/ariesve
 RELEASENAME="broodROM-JB-Release-4.zip"
-# Copyright 2013 broodplank.net
 
+# Copyright 2013 broodplank.net
+# REV2 (Release 4)
 
 # Number of simultaneous jobs
 
+#CPU:
 JOBS=5 
 
-#CPU:
 #Single core: -j2
 #Dual core: -j3
 #Quad Core: -j5
@@ -19,7 +22,7 @@ JOBS=5
 
 clear
 clear
-echo " "	
+echo " "  
 echo "----------------------------------------"
 echo "-     broodROM Jellybean Release 4     -"
 echo "-          Auto build script           -"
@@ -34,7 +37,12 @@ echo "- Preparing environment for compiling  -"
 echo "----------------------------------------"
 echo " "
 busybox sleep 1
+echo "Loading initial environment setup"
+echo " "
 . build/envsetup.sh
+echo "Ordering full_ariesve-userdebug for lunch"
+echo "Ignore the dependencies not found warning"
+echo " "
 lunch full_ariesve-userdebug
 
 echo " "	
@@ -44,7 +52,9 @@ echo "-  Number of simultaneous jobs: ${JOBS}      -"
 echo "----------------------------------------"
 echo " "
 busybox sleep 1
-make -j${JOBS}
+echo "Building!"
+echo " "
+#make -j${JOBS}
 
 echo " "	
 echo "----------------------------------------"
@@ -52,9 +62,13 @@ echo "-       Manipulating output...         -"
 echo "----------------------------------------"
 echo " "
 busybox sleep 1
-rm -Rf ${ARIESVEDIR}/system/xbin/*
-cp -Rf ${HOMEDIR}/build/broodrom/xbin/* ${ARIESVEDIR}/system/xbin/*
+echo "Replacing contents of xbin"
+rm -Rf ${ARIESVEDIR}/system/xbin
+cp -Rf ${HOMEDIR}/build/broodrom/xbin ${ARIESVEDIR}/system/xbin
+echo "Replacing kernel"
 cp -f ${ARIESVEDIR}/system/etc/broodrom/boot_ocuv.img ${ARIESVEDIR}/boot.img
+echo "Placing META-INF folder"
+rm -Rf ${ARIESVEDIR}/META-INF
 cp -Rf ${HOMEDIR}/build/broodrom/recovery/META-INF ${ARIESVEDIR}/META-INF
 
 
@@ -64,13 +78,18 @@ echo "-     Packing final OTA zip file       -"
 echo "----------------------------------------"
 echo " "
 busybox sleep 1
+echo "Preparing zip contents:"
 cd ${ARIESVEDIR}
 rm -Rf autobuild
 mkdir autobuild
-cp boot.img autobuild/
+echo "Copy boot.img"
+cp boot.img autobuild/boot.img
+echo "Copy system folder"
 cp -R system autobuild/system
+echo "Copy META-INF folder"
 cp -R META-INF autobuild/META-INF
 cd autobuild
+echo "Zipping all"
 zip -r ${RELEASENAME} .
 mv -f ${RELEASENAME} ${HOMEDIR}/build/broodrom/${RELEASENAME}
 
@@ -82,26 +101,19 @@ echo " "
 busybox sleep 1
 
 cd ${HOMEDIR}/build/broodrom
+echo "Signing, please wait..."
 java -jar signapk.jar testkey.x509.pem testkey.pk8 ${RELEASENAME} signed-${RELEASENAME}
 mv -f signed-${RELEASENAME} ${HOMEDIR}/signed-${RELEASENAME}
 rm -f ${RELEASENAME} 
+echo "Signing done!" 
 
 
-
-echo " "	
-echo " All operations done!		"
-echo " Signed Zip can be found in root folder"
 echo " "
+echo " "
+echo " ---------------------------------------------"	
+echo " - All operations done!                      -"
+echo " - Signed Zip can be found in root folder    -"
+echo " ---------------------------------------------"
 
 busybox sleep 5
 exit
-
-
-
-
-
-
-
-
-
-
