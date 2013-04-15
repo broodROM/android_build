f229d68c814def44374b21ae5203f6008cb8ebc1
broodplank <guichardflorent@gmail.com>
2013-04-15 23:58:53 +0200

    Update to Rev3, Add odin support

--- build.sh
+++ build.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 
+# ---------------------------------------------------------
+# Copyright 2013 broodplank.net
+# REV3 (Release 4)
+# ---------------------------------------------------------
+
 # Init Vars
 HOMEDIR=${PWD}
 ARIESVEDIR=${HOMEDIR}/out/target/product/ariesve
 RELEASENAME="broodROM-JB-Release-4.zip"
 
-# Copyright 2013 broodplank.net
-# REV2 (Release 4)
-
-# Number of simultaneous jobs
-
-#CPU:
-JOBS=5 
-
-#Single core: -j2
-#Dual core: -j3
-#Quad Core: -j5
-#Octa Core: -j9 (i7 Hyperthreading)
-#Octa Core: -j16 (Native AMD octa core)
+# ---------------------------------------------------------
+#
+# BUILD CONFIG
+#
+# ---------------------------------------------------------
+#
+# Main Configuration
+#
+  MAKEPARAM=""           # Example: MAKEPARAM="-k" 
+  JOBS=5                 # CPU Cores + 1 (also hyperthreading)
+  INCLUDERECOVERY=1      # Includes recovery.img in zip (0/1)
+#
+# Odin Configuration
+# All files besides system.img, boot.img, reovery.img and cache.img
+# should be placed in build/broodrom/odin to be included if 1 (below)
+#
+  BUILDODIN=1            # If 1, an odin package will be created
+                         # If 0, all options below will be ignored
+  ODINADDMODEM=0         # Add amss.mbn (modem) to odin package
+  ODINADDBOOT=0          # Add adsp.mbn, dbl.mbn, osbl.mbn (bootloader)
+  ODINADDPARAM=0         # Add EMMCBOOT.MBN and partition.bin
+#
+# ---------------------------------------------------------
 
 
 clear
 clear
-echo " "  
+echo " "	
 echo "----------------------------------------"
 echo "-     broodROM Jellybean Release 4     -"
 echo "-          Auto build script           -"
@@ -40,6 +55,7 @@ busybox sleep 1
 echo "Loading initial environment setup"
 echo " "
 . build/envsetup.sh
+echo " "
 echo "Ordering full_ariesve-userdebug for lunch"
 echo "Ignore the dependencies not found warning"
 echo " "
@@ -54,7 +70,7 @@ echo " "
 busybox sleep 1
 echo "Building!"
 echo " "
-make -j${JOBS}
+make -j${JOBS} ${MAKEPARAM}
 
 echo " "	
 echo "----------------------------------------"
@@ -67,9 +83,15 @@ rm -Rf ${ARIESVEDIR}/system/xbin
 cp -Rf ${HOMEDIR}/build/broodrom/xbin ${ARIESVEDIR}/system/xbin
 echo "Replacing kernel"
 cp -f ${ARIESVEDIR}/system/etc/broodrom/boot_ocuv.img ${ARIESVEDIR}/boot.img
-echo "Placing META-INF folder"
-rm -Rf ${ARIESVEDIR}/META-INF
-cp -Rf ${HOMEDIR}/build/broodrom/recovery/META-INF ${ARIESVEDIR}/META-INF
+if [[ "$INCLUDERECOVERY" == "1" ]]; then
+	echo "Placing META-INF folder"
+	rm -Rf ${ARIESVEDIR}/META-INF
+	cp -Rf ${HOMEDIR}/build/broodrom/recovery/META-INF1 ${ARIESVEDIR}/META-INF
+else
+	echo "Placing META-INF folder"
+	rm -Rf ${ARIESVEDIR}/META-INF
+	cp -Rf ${HOMEDIR}/build/broodrom/recovery/META-INF ${ARIESVEDIR}/META-INF
+fi;
 
 
 echo " "	
@@ -84,6 +106,10 @@ rm -Rf autobuild
 mkdir autobuild
 echo "Copy boot.img"
 cp boot.img autobuild/boot.img
+if [[ "$INCLUDERECOVERY" == "1" ]]; then
+echo "Copy recovery.img"
+cp -f recovery.img autobuild/recovery.img
+fi;
 echo "Copy system folder"
 cp -R system autobuild/system
 echo "Copy META-INF folder"
@@ -104,16 +130,73 @@ cd ${HOMEDIR}/build/broodrom
 echo "Signing, please wait..."
 java -jar signapk.jar testkey.x509.pem testkey.pk8 ${RELEASENAME} signed-${RELEASENAME}
 mv -f signed-${RELEASENAME} ${HOMEDIR}/signed-${RELEASENAME}
-rm -f ${RELEASENAME} 
+rm -f ${RELEASENAME}
 echo "Signing done!" 
 
 
 echo " "
 echo " "
 echo " ---------------------------------------------"	
-echo " - All operations done!                      -"
+echo " - CWM Zip creation process completed        -"
 echo " - Signed Zip can be found in root folder    -"
 echo " ---------------------------------------------"
 
-busybox sleep 5
+busybox sleep 3
+
+if [[ "$BUILDODIN" == "1" ]]; then
+	rm -Rf ${HOMEDIR}/broodROM-Release-4.tar.md5
+        rm -Rf ${ARIESVEDIR}/autobuildodin
+	echo " "	
+	echo "----------------------------------------"
+	echo "-     Performing Additional Tasks      -"
+	echo "----------------------------------------"
+	echo "-                                      - "
+	echo "- Build Odin Package:                  -"
+	echo "- Include Modem: ${ODINADDMODEM}                     -"
+	echo "- Include Bootloader: ${ODINADDBOOT}                -"
+	echo "- Include Param: ${ODINADDPARAM}                     -"
+	echo "----------------------------------------"
+	echo " "
+	busybox sleep 5
+        echo "Preparing files for packaging"
+        echo " "
+	rm -Rf ${ARIESVEDIR}/autobuildodin
+	mkdir ${ARIESVEDIR}/autobuildodin
+	cp ${ARIESVEDIR}/system.img ${ARIESVEDIR}/autobuildodin/system.img
+	cp ${ARIESVEDIR}/system/etc/broodrom/boot_ocuv.img ${ARIESVEDIR}/autobuildodin/boot.img
+	cp ${ARIESVEDIR}/recovery.img ${ARIESVEDIR}/autobuildodin/recovery.img
+        echo "Creating empty cache file"
+	dd if=/dev/zero of=${ARIESVEDIR}/autobuildodin/cache.img bs=1K count=102400
+		if [[ "$ODINADDMODEM" == "1" ]]; then
+			cp ${HOMEDIR}/build/broodrom/odin/amss.mbn ${ARIESVEDIR}/autobuildodin/amss.mbn
+			$ODINMODEM = "amss.mbn"
+		fi;
+		if [[ "$ODINADDBOOT" == "1" ]]; then
+			cp ${HOMEDIR}/build/broodrom/odin/adsp.mbn ${ARIESVEDIR}/autobuildodin/adsp.mbn
+			cp ${HOMEDIR}/build/broodrom/odin/osbl.mbn ${ARIESVEDIR}/autobuildodin/osbl.mbn
+			cp ${HOMEDIR}/build/broodrom/odin/dbl.mbn ${ARIESVEDIR}/autobuildodin/dbl.mbn
+            $ODINBOOT = "adsp.mbn osbl.mbn dbl.mbn"
+		fi;
+		if [[ "$ODINADDPRAM" == "1" ]]; then
+			cp ${HOMEDIR}/build/broodrom/odin/EMMCBOOT.MBN ${ARIESVEDIR}/autobuildodin/EMMCBOOT.MBN
+			cp ${HOMEDIR}/build/broodrom/odin/partition.bin ${ARIESVEDIR}/autobuildodin/partition.bin
+            $ODINPARAM = "EMMCBOOT.MBN partition.bin"
+		fi;
+	echo " "
+        echo "Packing odin files"
+        echo " "
+	cd ${ARIESVEDIR}/autobuildodin
+	tar -c boot.img recovery.img system.img cache.img ${ODINMODEM} ${ODINBOOT} ${ODINPARAM} > broodROM-Release-4.tar
+	echo " "
+        echo "Adding MD5 Sums"
+        echo " "    
+	md5sum -t broodROM-Release-4.tar >> broodROM-Release-4.tar
+	mv broodROM-Release-4.tar ${HOMEDIR}/broodROM-Release-4.tar.md5
+	echo " ---------------------------------------------"	
+	echo " - Odin package creation done!               -"
+	echo " - Odin One package can be found in root     -"
+	echo " ---------------------------------------------"
+	echo " "
+	echo " EXIT"
+fi;
 exit
