13d18e095db508034f380a43bae6446a2c9db516
Florent Guichard <guichardflorent@gmail.com>
2013-05-01 22:45:30 +0300

    Revert usage of special linaro toolchain

--- envsetup.sh
+++ envsetup.sh
@@ -148,8 +148,7 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-android-linux-4.4.3/bin
             ;;
-          #Use linaro toolchain for now, old entry (arm) toolchaindir=arm/arm-linux-androideabi-4.6/bin)
-           arm) toolchaindir=arm/linaro/bin
+          arm) toolchaindir=arm/arm-linux-androideabi-4.6/bin
             ;;
         *)
             echo "Can't find toolchain for unknown architecture: $ARCH"
@@ -166,8 +165,7 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-eabi-4.4.3/bin
             ;;
-        #Use linaro toolchain for now, old entry (arm) toolchaindir=arm/arm-eabi-4.6/bin)
-        arm) toolchaindir=arm/linaro/bin
+        arm) toolchaindir=arm/arm-eabi-4.6/bin
             ;;
         *)
             echo "Can't find toolchain for unknown architecture: $ARCH"
